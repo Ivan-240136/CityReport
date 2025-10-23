f34d18368ed199f7f19b65b73bb0ef4f7677d90b
Ivan-240136 <240136@utxicotepec.edu.mx>
2025-10-23 13:30:12 -0600

    Actualizacion BD

--- conexion.php
+++ conexion.php
@@ -1,14 +1,12 @@
 <?php
-define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'tu_base_de_datos');
-define('DB_USER', 'tu_usuario');
-define('DB_PASS', 'tu_contraseña');
+
+$SUPABASE_URL = getenv('SUPABASE_URL');  
+$SUPABASE_ANON_KEY = getenv('SUPABASE_ANON_KEY');  
 
 try {
-    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $conn = new PDO("pgsql:host=$SUPABASE_URL;dbname=postgres", "postgres", $SUPABASE_ANON_KEY);  
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
-
